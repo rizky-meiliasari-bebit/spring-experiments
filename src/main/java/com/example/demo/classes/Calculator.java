197cde3a78d440d2e77d6f22a665653852e5b1ec
rizky-meiliasari-bebit <putri3039@gmail.com>
2022-01-11 20:41:41 +0900

    fix

--- src/main/java/com/example/demo/classes/Calculator.java
+++ src/main/java/com/example/demo/classes/Calculator.java
@@ -15,9 +15,9 @@ public class Calculator {
   }
 
   public int divide(int a, int b) {
-    if (b == 0) {
-      return 0;
-    }
+//    if (b == 0) {
+//      return 0;
+//    }
     return a / b;
   }
 
